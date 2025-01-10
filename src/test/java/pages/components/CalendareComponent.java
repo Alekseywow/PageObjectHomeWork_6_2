57ff425e294042157931d52660d76c5e3b66467a
Aleksey Danilov <danilov20022@gmail.com>
2025-01-10 10:57:31 +0500

    added Part_1

--- src/test/java/pages/components/CalendareComponent.java
+++ src/test/java/pages/components/CalendareComponent.java
@@ -4,8 +4,8 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class CalendareComponent {
     public void setDate(String day, String month, String year) {
-        $(".react-datepicker__month-select").selectOption("September");
-        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
     }
 }
